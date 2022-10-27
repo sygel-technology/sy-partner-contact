e929759ad3effd7f6dfe19736e6564c6d81b43a1
manu <manuel.regidor@sygel.es>
2022-10-27 12:30:39 +0200

    T-3402[ADD]contact_referred, campo contacto referido

---
+++ contact_referred/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Contact Referred',
+    'version': '15.0.1.0.0',
+    'author': 'Sygel',
+    'category': 'Contact',
+    'summary': "Referred Contact Field.",
+    'website': 'https://www.sygel.es',
+    'depends': [
+        'base',
+    ],
+    'data': [
+     'views/res_partner_views.xml'
+    ],
+}
