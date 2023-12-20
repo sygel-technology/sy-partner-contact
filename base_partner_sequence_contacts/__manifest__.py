a763fd22f5ccf1140b4557c7d888e385be03f69e
manu <manuel.regidor@sygel.es>
2023-12-20 15:57:41 +0100

    T-5338[ADD]base_partner_sequence_contacts

---
+++ base_partner_sequence_contacts/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Base Partner Sequence",
+    "summary": "Apply sequence to all contacts",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Sygel",
+    "category": "Contact",
+    "depends": [
+        "base_partner_sequence",
+    ],
+    "installable": True,
+}
