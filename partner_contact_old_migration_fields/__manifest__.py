6c2c08fc1c5bfc3d76b7073644393ce179041691
Alberto Martínez <alberto.martinez@sygel.es>
2024-02-14 15:45:29 +0100

    T-5625[MIG][17.0]partner_contact_old_migration_fields

--- partner_contact_old_migration_fields/__manifest__.py
+++ partner_contact_old_migration_fields/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Partner Contact Old Migration Fields",
     "summary": "Partner Contact Old Migration Fields",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Contact",
     "website": "https://www.sygel.es",
     "author": "Sygel, Odoo Community Association (OCA)",
