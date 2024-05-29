c9ea5e58f15df21c856580db3a92783f5472af48
sygel <alberto.martinez@sygel.es>
2024-05-29 13:48:20 +0200

    [T-6038] [ADD] partner_industrial_estate module

---
+++ partner_industrial_estate/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2024 Alberto Martínez <alberto.martinez@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Partner Industrial Estate',
+    'summary': 'Adds the partner industrial estate fields on contacts',
+    'version': '17.0.1.0.0',
+    'category': 'Partner Management',
+    'website': 'https://sygel.es',
+    'author': 'Sygel',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'contacts',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_partner_industrial_estate_views.xml',
+        'views/res_partner_views.xml',
+    ],
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
+}
