e57796d2e71a90ef5e360b7c66b93ea963bc2eac
sygel <alberto.martinez@sygel.es>
2024-06-07 13:20:31 +0200

    [T-6042][ADD] partner_code_program_territory aditional modules: partner_code_program_territory_account partner_code_program_territory_purchase partner_code_program_territory_sale

---
+++ partner_code_program_territory_purchase/models/purchase_order.py
@@ -0,0 +1,12 @@
+# Copyright 2024 Alberto Martínez <alberto.martinez@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class PurchaseOrder(models.Model):
+    _name = "purchase.order"
+    _inherit = [
+        "purchase.order",
+        "res.partner.code.program.territory.mixin"
+    ]
---
+++ partner_code_program_territory_purchase/models/purchase_report.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Alberto Martínez <alberto.martinez@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class PurchaseReport(models.Model):
+    _inherit = "purchase.report"
+
+    code_program_territory_id = fields.Many2one(
+        string="Code Program Territory",
+        comodel_name="res.partner.code.program.territory",
+    )
+
+    def _select(self):
+        return super(PurchaseReport, self)._select() + ", partner.code_program_territory_id as code_program_territory_id"
+
+    def _group_by(self):
+        return super(PurchaseReport, self)._group_by() + ", partner.code_program_territory_id"
