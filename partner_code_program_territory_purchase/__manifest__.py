62f60c1234e0ec41f703db2561deef08b7f21d60
sygel <alberto.martinez@sygel.es>
2024-06-07 13:20:31 +0200

    [T-6042][ADD] partner_code_program_territory aditional modules: partner_code_program_territory_account partner_code_program_territory_purchase partner_code_program_territory_sale

---
+++ partner_code_program_territory_purchase/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2024 Alberto Martínez <alberto.martinez@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Partner Code Program Purchase',
+    'summary': 'Adds the partner code program territory fields on purchases',
+    'version': '17.0.1.0.0',
+    'category': 'Partner Management',
+    'website': 'https://sygel.es',
+    'author': 'Sygel',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'partner_code_program_territory',
+        'purchase'
+    ],
+    'data': [
+        'views/purchase_order.xml',
+        'views/purchase_report.xml',
+    ],
+}
