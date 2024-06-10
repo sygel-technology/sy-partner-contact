f79be0ebc095e6a56d12205e30642148eb8787cb
sygel <alberto.martinez@sygel.es>
2024-06-10 16:54:39 +0200

    [T-6040][ADD] partner_code_program aditional modules     partner_code_program_sale     partner_code_program_account     partner_code_program_purchase

---
+++ partner_code_program_sale/models/sale_order.py
@@ -0,0 +1,12 @@
+# Copyright 2024 Alberto Martínez <alberto.martinez@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class SaleOrder(models.Model):
+    _name = "sale.order"
+    _inherit = [
+        "sale.order",
+        "res.partner.code.program.mixin"
+    ]
