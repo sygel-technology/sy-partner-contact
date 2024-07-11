932339505ea1be021759ccceff347ec74e76ecf1
Manuel Florido <manuel.florido@sygel.es>
2024-07-11 09:21:19 +0200

    [T-5944][IMP] contact_referred,partner_contact_old_acc_number,partner_contact_old_migration_fields,partner_vat_required:pre-commit auto fixes

--- partner_contact_old_migration_fields/__manifest__.py
+++ partner_contact_old_migration_fields/__manifest__.py
@@ -6,16 +6,12 @@
     "summary": "Partner Contact Old Migration Fields",
     "version": "15.0.1.0.0",
     "category": "Contact",
-    "website": "https://www.sygel.es",
+    "website": "https://github.com/sygel-technology/sy-partner-contact",
     "author": "Sygel, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        'base',
-        'contacts',
-        'base_old_migration_fields'
-    ],   
+    "depends": ["base", "contacts", "base_old_migration_fields"],
     "data": [
         "views/res_partner_views.xml",
     ],
