44d3cd166fb533df710243aa5831f34de1d4d399
Manuel Florido <manuel.florido@sygel.es>
2024-07-11 09:21:19 +0200

    [T-5944][IMP] contact_referred,partner_contact_old_acc_number,partner_contact_old_migration_fields,partner_vat_required:pre-commit auto fixes

--- contact_referred/__manifest__.py
+++ contact_referred/__manifest__.py
@@ -2,17 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Contact Referred',
-    'version': '15.0.1.0.0',
-    'author': 'Sygel',
-    'category': 'Contact',
-    'summary': "Referred Contact Field.",
-    'website': 'https://www.sygel.es',
-    'depends': [
-        'base',
+    "name": "Contact Referred",
+    "version": "15.0.1.0.0",
+    "author": "Sygel",
+    "category": "Contact",
+    "summary": "Referred Contact Field.",
+    "website": "https://github.com/sygel-technology/sy-partner-contact",
+    "depends": [
+        "base",
     ],
-    'data': [
-     'views/res_partner_views.xml'
-    ],
-    'license': 'LGPL-3',
+    "data": ["views/res_partner_views.xml"],
+    "license": "LGPL-3",
 }
