8bf3db093882343bc935026cc5644ff58968ca85
Roger <roger.sans@sygel.es>
2024-07-22 13:51:40 +0200

    [FIX]pre-commit fix errors

--- partner_code_program/models/res_partner_code_program.py
+++ partner_code_program/models/res_partner_code_program.py
@@ -10,9 +10,8 @@ class ResPartnerCodeProgram(models.Model):
 
     _rec_names_search = ["name", "code"]
 
-    name = fields.Char(string="Name", required=True, translate=True)
+    name = fields.Char(required=True, translate=True)
     code = fields.Char(
-        string="Code",
         required=True,
     )
     res_partner_count = fields.Integer(
