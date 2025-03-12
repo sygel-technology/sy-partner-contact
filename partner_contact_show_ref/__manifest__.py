fca20f0b84aecb780bfb3c9b0fc2359ec0d6919f
sygel <alberto.martinez@sygel.es>
2025-03-12 18:01:13 +0100

    [T-6642][ADD] partner_contact_show_ref

---
+++ partner_contact_show_ref/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 Alberto Martínez <alberto.martinez@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Contact Show Ref",
+    "summary": "Shows contact ref in contact name",
+    "version": "17.0.1.0.0",
+    "category": "Tools",
+    "website": "https://github.com/sygel-technology/sy-partner-contact",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+}
