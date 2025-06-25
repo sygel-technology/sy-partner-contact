ef1ff815f682af1f7cfdb56fb61445fec776d296
Angel Rivas <angel.rivas@sygel.es>
2025-06-25 13:09:13 +0200

    [T-8592][MIG] base_location_code_program_territory: migration to 18.0

--- base_location_code_program_territory/__manifest__.py
+++ base_location_code_program_territory/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Base Location Code Program Territory",
     "summary": "Associate ZIPs to code programs and code territories",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Partner Management",
     "website": "https://github.com/sygel-technology/sy-partner-contact",
     "author": "Sygel",
---
+++ base_location_code_program_territory/readme/DESCRIPTION.md
@@ -0,0 +1,2 @@
+This module allows you to associate ZIPs to code programs and code programs territories.
+It also adds those fields to the partner address autocompletion
